d82f0adf63ae83a82bf4ad70a23b0dfe686c3a3e
Nikhil krishna Marella <marellanikhilkrishnareddy@gmail.com>
2025-12-12 14:39:56 +0530

    Create base_page.py

---
+++ base_page.py
@@ -0,0 +1,29 @@
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+class BasePage:
+    def __init__(self, driver, timeout=10):
+        self.driver = driver
+        self.wait = WebDriverWait(driver, timeout)
+
+    def open(self, url):
+        self.driver.get(url)
+
+    def find(self, locator):
+        return self.wait.until(EC.presence_of_element_located(locator))
+
+    def click(self, locator):
+        el = self.wait.until(EC.element_to_be_clickable(locator))
+        el.click()
+
+    def type_text(self, locator, text):
+        el = self.wait.until(EC.presence_of_element_located(locator))
+        el.clear()
+        el.send_keys(text)
+
+    def is_element_visible(self, locator):
+        try:
+            self.wait.until(EC.visibility_of_element_located(locator))
+            return True
+        except:
+            return False
